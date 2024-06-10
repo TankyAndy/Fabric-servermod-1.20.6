64ae54a13b507555cb2091fa5a926e816078df5d
TankyAndy <andyzhqli@gmail.com>
2024-06-10 15:52:55 -0400

    test change

--- src/main/java/net/andy/servermobs/ServerMobs.java
+++ src/main/java/net/andy/servermobs/ServerMobs.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 public class ServerMobs implements ModInitializer {
     public static final String MOD_ID = "servermobs";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+//testing changes
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
