74d207cde204542497aa32de09044c5bd6370aad
TankyAndy <andyzhqli@gmail.com>
2024-06-11 01:57:11 -0400

    test change

--- src/main/java/net/andy/servermobs/item/ModItems.java
+++ src/main/java/net/andy/servermobs/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.andy.servermobs.item;
 
 import net.andy.servermobs.ServerMobs;
+import net.andy.servermobs.item.custom.FlashlightItem;
 import net.fabricmc.fabric.api.item.v1.FabricItem;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
@@ -12,9 +13,10 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
     public static final Item FLASHLIGHTBETA = registerItem("flashlightbeta", new Item(new Item.Settings()));
-
+    public static final Item FLASHLIGHT = registerItem("flashlight", new FlashlightItem(new Item.Settings()));
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.add(FLASHLIGHTBETA);
+        entries.add(FLASHLIGHT);
     }
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(ServerMobs.MOD_ID, name), item);
--- src/main/java/net/andy/servermobs/item/custom/FlashlightItem.java
+++ src/main/java/net/andy/servermobs/item/custom/FlashlightItem.java
@@ -1,6 +1,26 @@
 package net.andy.servermobs.item.custom;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.TorchBlock;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.decoration.ArmorStandEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
+import net.minecraft.world.World;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import static net.minecraft.entity.EntityType.ARMOR_STAND;
 
 public class FlashlightItem extends Item {
 
@@ -8,6 +28,21 @@ public class FlashlightItem extends Item {
         super(settings);
     }
 
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        user.playSound(SoundEvents.BLOCK_LEVER_CLICK, 1.0F, 1.0F);
 
+        return super.use(world, user, hand);
+    }
 
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        World world = context.getWorld();
+        BlockPos pos = context.getBlockPos().offset(context.getSide());
+
+        if (!world.isClient) {
+            world.setBlockState(pos, Blocks.TORCH.getDefaultState());
+        }
+
+        return ActionResult.SUCCESS;
+    }
 }
