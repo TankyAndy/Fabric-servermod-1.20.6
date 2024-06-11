ac2fa502313e192b48f695d6a68e9f6b4bbef3f4
TankyAndy <andyzhqli@gmail.com>
2024-06-11 02:16:16 -0400

    test change

--- src/main/java/net/andy/servermobs/item/custom/FlashlightItem.java
+++ src/main/java/net/andy/servermobs/item/custom/FlashlightItem.java
@@ -1,32 +1,34 @@
 package net.andy.servermobs.item.custom;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.TorchBlock;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.decoration.ArmorStandEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
-import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
 import net.minecraft.block.Blocks;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
-import static net.minecraft.entity.EntityType.ARMOR_STAND;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.server.world.ServerWorld;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class FlashlightItem extends Item {
+    private static final long TORCH_LIFETIME = 10;
+    private final Map<Long, Long> torchTimers = new HashMap<Long, Long>();
 
     public FlashlightItem(Settings settings) {
         super(settings);
+        ServerTickEvents.END_WORLD_TICK.register(this::onWorldTick);
     }
+
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
@@ -38,6 +40,39 @@ public class FlashlightItem extends Item {
     public ActionResult useOnBlock(ItemUsageContext context) {
         World world = context.getWorld();
         BlockPos pos = context.getBlockPos().offset(context.getSide());
+        Block block = Blocks.TORCH;
+
+        if (!world.isClient) {
+            world.setBlockState(pos, block.getDefaultState());
+            torchTimers.put(pos.asLong(), world.getTime() + TORCH_LIFETIME);
+        }
+
+        return ActionResult.SUCCESS;
+    }
+
+    private void onWorldTick(ServerWorld world) {
+        Iterator<Map.Entry<Long, Long>> iterator = torchTimers.entrySet().iterator();
+        long currentTime = world.getTime();
+
+        while (iterator.hasNext()) {
+            Map.Entry<Long, Long> entry = iterator.next();
+            BlockPos pos = BlockPos.fromLong(entry.getKey());
+            long despawnTime = entry.getValue();
+
+            if (currentTime >= despawnTime) {
+                if (world.getBlockState(pos).getBlock() == Blocks.TORCH) {
+                    world.setBlockState(pos, Blocks.AIR.getDefaultState());
+                }
+                iterator.remove();
+            }
+        }
+    }
+}
+
+    /*
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        World world = context.getWorld();
+        BlockPos pos = context.getBlockPos().offset(context.getSide());
 
         if (!world.isClient) {
             world.setBlockState(pos, Blocks.TORCH.getDefaultState());
@@ -45,4 +80,6 @@ public class FlashlightItem extends Item {
 
         return ActionResult.SUCCESS;
     }
-}
+    }
+     */
+
